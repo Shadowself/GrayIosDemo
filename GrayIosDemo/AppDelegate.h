83763b52644e0d5e512ec09443aa5b04022c3bf8
guoyu.zhang <guoyu.zhang@husor.com>
2016-09-02 16:43:48 +0800

    Initial Commit

---
+++ GrayIosDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GrayIosDemo
+//
+//  Created by shadow on 16/9/2.
+//  Copyright © 2016年 shadow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GrayIosDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GrayIosDemo
+//
+//  Created by shadow on 16/9/2.
+//  Copyright © 2016年 shadow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
