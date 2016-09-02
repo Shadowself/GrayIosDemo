44f12d52a4961f9c85cf38203e31a2e7fcbe591d
guoyu.zhang <guoyu.zhang@husor.com>
2016-09-02 19:55:20 +0800

    添加跳转功能

---
+++ GrayIosDemo/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  GrayIosDemo
+//
+//  Created by shadow on 16/9/2.
+//  Copyright © 2016年 shadow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
---
+++ GrayIosDemo/ViewController3.h
@@ -0,0 +1,13 @@
+//
+//  ViewController3.h
+//  GrayIosDemo
+//
+//  Created by shadow on 16/9/2.
+//  Copyright © 2016年 shadow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController3 : UIViewController
+
+@end
